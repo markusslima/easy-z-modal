580e131c4ab4b70834f9d0b2bd9167e1000c19cb
Markus Lima <markusslima@gmail.com>
2015-07-15 11:21:28 -0300

    Correction closable Add data attributes Update example

--- src/ezmodal.js
+++ src/ezmodal.js
@@ -4,24 +4,26 @@
  * github: https://github.com/markusslima/ezmodal
  *
  * Copyright (c) 2015 Markus Vinicius da Silva Lima
- * Version 0.1.0
+ * Version 0.1.1
  * Licensed under the MIT license.
  */
 (function ($) {
     "use strict";
-    
+
     $(window).on('keyup', function (event) {
         if (event.keyCode === 27) {
-            var data = $('.ezmodal').data('ezmodal');
-            if (data.options.closable) {
-                $('.ezmodal').ezmodal('hide');
-            }
+            $('.ezmodal').each(function () {
+                if ($(this).ezmodal('isVisible')) {
+                    if ($(this).data('ezmodal').options.closable) {
+                        $(this).ezmodal('hide');
+                    }
+                }
+            });
         }
     });
 
     $(document).on('click', '.ezmodal', function () {
-        var data = $(this).data('ezmodal');
-        if (data.options.closable) {
+        if ($(this).data('ezmodal').options.closable) {
             $(this).ezmodal('hide');
         }
     });
@@ -29,7 +31,7 @@
     $(document).on('click', '.ezmodal .ezmodal-container', function (event) {
         event.stopPropagation();
     });
-    
+
     $(document).on('click', '[data-dismiss="ezmodal"]', function () {
         $(this).parent().parent().parent().ezmodal('hide');
     });
@@ -39,15 +41,12 @@
     });
 
     var EZmodal = function (element, options) {
-        this.options = options;
-        this.$element = $(element);
-    };
+			this.options = options;
+			this.$element = $(element);
+		},
+		old;
 
     EZmodal.prototype = {
-        width: function () {
-            
-        },
-        
         show: function () {
             this.$element.show();
             this.options.onShow();
@@ -59,11 +58,11 @@
         },
 
         isVisible: function () {
-            return this.$element.css('visibility') === 'visible' ? true : false;
+            return this.$element.css('display') === 'block' ? true : false;
         },
         
         constructor: function () {
-            var _this = this,
+            var width = this.options.width,
                 container = this.$element.find('.ezmodal-container');
                 
             if (this.options.autoOpen) {
@@ -72,25 +71,25 @@
             
             if (Number(this.options.width)) {
                 container.css({
-                    'width':  _this.options.width+'px'
+                    'width': width + 'px'
                 });
             } else {
-                switch (_this.options.width) {
-                    case 'small':
-                        container.css({'width': '40%'});
-                        break;
-                    case 'medium':
-                        container.css({'width': '75%'});
-                        break;
-                    case 'full':
-                        container.css({'width': '95%'});
-                        break;
+                switch (width) {
+                case 'small':
+					container.css({'width': '40%'});
+					break;
+				case 'medium':
+					container.css({'width': '75%'});
+					break;
+				case 'full':
+					container.css({'width': '95%'});
+					break;
                 }
             }
         }
     };
 
-    var old = $.fn.ezmodal;
+    old = $.fn.ezmodal;
 
     $.fn.ezmodal = function (option, value) {
         var get = '',
@@ -128,4 +127,16 @@
         $.fn.ezmodal = old;
         return this;
     };
+    
+    $(function () {
+		$('.ezmodal').each(function () {
+			var $this = $(this),
+                options = {
+					'width' : $this.attr('ezmodal-width'),
+					'closable' : $this.attr('ezmodal-closable') === 'false' ? false : true,
+					'autoOpen' : $this.attr('ezmodal-autoopen') === 'true' ? true : false
+				};
+			$this.ezmodal(options);
+		});
+	});
 })(window.jQuery);
